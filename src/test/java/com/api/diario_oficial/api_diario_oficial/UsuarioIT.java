862acb8afdb4f8860654b197a62e2521ab433cd4
jota santos <gestald118@email.com>
2024-06-08 19:21:40 -0400

    Implementando testes de integração para Listagem de usuario

--- src/test/java/com/api/diario_oficial/api_diario_oficial/UsuarioIT.java
+++ src/test/java/com/api/diario_oficial/api_diario_oficial/UsuarioIT.java
@@ -5,7 +5,10 @@ import com.api.diario_oficial.api_diario_oficial.config.ApiPath;
 import com.api.diario_oficial.api_diario_oficial.entity.Usuario;
 import com.api.diario_oficial.api_diario_oficial.enums.Role;
 import com.api.diario_oficial.api_diario_oficial.jwt.JwtUserDetails;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,8 +22,8 @@ import java.time.LocalDateTime;
 
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = "/sql/usuarios/usuarios-truncate.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+// @Sql(scripts = "/sql/usuarios/usuarios-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+// @Sql(scripts = "/sql/usuarios/usuarios-truncate.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class UsuarioIT {
 
 
@@ -65,29 +68,44 @@ public class UsuarioIT {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
     }
 
+    @SneakyThrows
     @Test
     void shouldReturnListUsersWhenUserAndPasswordValid() {
         // Login and get JWT token
         JwtUserDetails jwtUserDetails = getValidCredentialsLogin();
 
         ResponseEntity<String> loginResponse = restTemplate.postForEntity(ApiPath.AUTENTICAR, jwtUserDetails, String.class);
-        String jwtToken = loginResponse.getBody();
-
-        log.error("TOKEN INFORMADO : " + jwtToken);
-        log.error("loginResponse: " + loginResponse);
-
-        // Ensure token is not null
-        assertThat(jwtToken).isNotNull();
+        JSONObject loginResponseJon = new JSONObject(loginResponse.getBody());
 
         // Use JWT token to access protected endpoint
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + jwtToken);
+        headers.set("Authorization", "Bearer " + loginResponseJon.getString("token"));
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         ResponseEntity<String> response = restTemplate.exchange(ApiPath.USUARIOS, HttpMethod.GET, entity, String.class);
+        JSONObject responseJsonList = new JSONObject(response.getBody());
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo("This is a protected endpoint");
+        assertThat(response.getBody()).isNotEmpty();
+
+        // verificandopaginação
+        assertThat(responseJsonList.has("content")).isTrue();
+        assertThat(responseJsonList.has("pageable")).isTrue();
+        assertThat(responseJsonList.has("totalPages")).isTrue();
+        assertThat(responseJsonList.getString("totalPages")).asInt().isEqualTo(6);
+        assertThat(responseJsonList.has("totalElements")).isTrue();
+        assertThat(responseJsonList.getString("totalElements")).asInt().isEqualTo(53);
+        assertThat(responseJsonList.has("last")).isTrue();
+        assertThat(responseJsonList.has("first")).isTrue();
+        assertThat(responseJsonList.has("size")).isTrue();
+        assertThat(responseJsonList.has("number")).isTrue();
+        assertThat(responseJsonList.has("sort")).isTrue();
+        assertThat(responseJsonList.has("numberOfElements")).isTrue();
+        assertThat(responseJsonList.has("empty")).isTrue();
+
+        // verificando se a lista não é vazia
+        JSONArray contentArray = responseJsonList.getJSONArray("content");
+        assertThat(contentArray).isNotNull();
     }
 
 
