aaaaf403093c924b959ce199da95dfb1e880d592
jota santos <gestald118@email.com>
2024-06-17 08:58:13 -0400

    Novas constantes para api path

--- src/main/java/com/api/diario_oficial/api_diario_oficial/config/ApiPath.java
+++ src/main/java/com/api/diario_oficial/api_diario_oficial/config/ApiPath.java
@@ -5,6 +5,8 @@ public class ApiPath {
     public static final String LOGIN = "/api/v1/login";
     public static final String AUTENTICAR = "/api/v1/auth";
     public static final String CLIENTES = "/api/v1/clientes";
+    public static final String ENDERECOS = "/api/v1/enderecos";
+    public static final String ORGAO_GOVERNAMENTAL = "/api/v1/orgao-governamentals";
     public static final String ROLES = "/api/v1/roles";
     public static final String USUARIOS = "/api/v1/usuarios";
 }
