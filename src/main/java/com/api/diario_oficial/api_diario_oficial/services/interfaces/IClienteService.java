50ec1954b94428c5b38c372e1a80da6db1aac70c
jota santos <gestald118@email.com>
2024-06-17 08:55:48 -0400

    Implementando classes services

--- src/main/java/com/api/diario_oficial/api_diario_oficial/services/interfaces/IClienteService.java
+++ src/main/java/com/api/diario_oficial/api_diario_oficial/services/interfaces/IClienteService.java
@@ -4,7 +4,9 @@ import com.api.diario_oficial.api_diario_oficial.entity.Cliente;
 import com.api.diario_oficial.api_diario_oficial.web.dtos.clientes.ClienteSearchDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface IClienteService extends IServiceBase<Cliente, Long> {
 
     Page<Cliente> search(ClienteSearchDTO searchDto, Pageable pageable);
