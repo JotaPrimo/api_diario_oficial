3b2b93a8400b8d6a90a8a74463d0bee7e3e0f73a
jota santos <gestald118@email.com>
2024-10-17 08:51:17 -0400

    DTO's de OrgaoGovnernamental

--- src/main/java/com/api/diario_oficial/api_diario_oficial/web/dtos/orgao_governamental/OrgaoGovernamentalUpdateDto.java
+++ src/main/java/com/api/diario_oficial/api_diario_oficial/web/dtos/orgao_governamental/OrgaoGovernamentalUpdateDto.java
@@ -5,20 +5,19 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CNPJ;
 
-public record OrgaoGovernamentalCreateDTO(
-
+public record OrgaoGovernamentalUpdateDto(
         @NotBlank(message = "Nome é um campo obrigatório")
         @Size(min = 5, max = 250, message = "Nome deve ter entre {min} e  {max} caracteres")
         String nome,
 
         @CNPJ
         @NotBlank(message = "CNPJ é um campo obrigatório")
-        String cnpj)
-{
-        public static OrgaoGovernamental toEntity(OrgaoGovernamentalCreateDTO dto) {
-                OrgaoGovernamental orgaoGovernamental = new OrgaoGovernamental();
-                orgaoGovernamental.setNome(dto.nome);
-                orgaoGovernamental.setCnpj(dto.cnpj);
-                return orgaoGovernamental;
-        }
+        String cnpj
+) {
+    public static OrgaoGovernamental transferData(OrgaoGovernamentalUpdateDto dto, OrgaoGovernamental orgaoGovernamental) {
+        // transferindo os dados do dto para a entity, antes de atualizar
+        orgaoGovernamental.setNome(dto.nome);
+        orgaoGovernamental.setCnpj(dto.cnpj);
+        return orgaoGovernamental;
+    }
 }
